dce212b3b23a6c4fc7b9c3302d10ab4ef80872e1
Fredrik <fredriep@student.matnat.uio.no>
2014-05-14 16:53:11 +0200

    made list of abbreviations ++

--- doc/latex2/Figures/tridiag.cpp
+++ doc/latex2/Figures/tridiag.cpp
@@ -1,19 +1,19 @@
-void tridiag(double *u, double *up, int N, double *a, double *b, double *c){
-    double *H = new double[N];
-    double *g = new double[N];
-    for(int i=0;i<N;i++){
+void tridiag(double *u, double *up, int n, double *a, double *b, double *c){
+    double *H = new double[n];
+    double *g = new double[n];
+    for(int i=0;i<n;i++){
     	H[i] = 0;
         g[i] = 0;
     }
     g[0] = up[0]/b[0];
     H[0] = c[0]/b[0];
-    for(int i=1; i<N; i++){
+    for(int i=1; i<n; i++){
         //forward substitution
         H[i] = -c[i]/(b[i] + a[i]*H[i-1]);
         g[i] = (up[i] - a[i]*g[i-1])/(b[i] + a[i]*H[i-1]);
     }
-    u[N-1] = g[N-1];
-    for(int i=(N-2);i>=0;i--){
+    u[n-1] = g[n-1];
+    for(int i=(n-2);i>=0;i--){
         //Backward substitution
         u[i] = g[i] - H[i]*u[i+1];
     }
